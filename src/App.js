435f0cf05f5195216c069a55b7c4b015f728e2df
shekhus <cpsatav@gmail.com>
2023-01-01 23:10:29 +0530

    connect Blockchain & frontend,App.js & Navigatio

--- src/App.js
+++ src/App.js
@@ -15,12 +15,31 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+   }
+   useEffect(() => {
+     loadBlockchainData ()
+       },[])
+
   return (
     <div>
 
+<Navigation account={account} setAccount={setAccount} />
+
+<Search/>
+
       <div className='cards__section'>
 
-        <h3>Welcome to Millow</h3>
+        <h3>Welcome to NoBroker</h3>
 
       </div>
 
